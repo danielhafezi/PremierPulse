8e07261c76a8af157b7636b5fa110d679c548fe6
danielhafezi <danielhafezian@gmail.com>
2024-05-12 14:29:48 +0000

    got rid of docker, made directories to use XAMPP

--- Task2/src/testDateTime.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $date = new DateTime('2018-11-20');
-    echo $date->format('d/m/Y') . '<br/>'; 
-    $date->modify('-2 Days'); 
-    echo $date->format('d/m/Y') . '<br/>'; 
-    $date->modify('-2 Days'); 
-    echo $date->format('d/m/Y'); 
-?>
