d1c697d519f24503973ad87f941002b8edddfc18
danielhafezi <danielhafezian@gmail.com>
2024-05-13 12:31:51 +0000

    Styling dashboard

--- Task2/dashboard.php
+++ Task2/dashboard.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Dashboard</title>
-<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="css/custom.css">
 </head>
 <body>
     <header>
